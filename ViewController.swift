5d6a4945418383f50c03ee247f169e916847e08b
Rushil Nagarsheth <rushil.nagarsheth@gmail.com>
2019-06-01 10:19:20 -0400

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicTac2
+//
+//  Created by Rushil Nagarsheth on 01/06/19.
+//  Copyright © 2019 Rushil Nagarsheth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
