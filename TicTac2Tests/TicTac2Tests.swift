230a4d17a35983efc5c258192f798097294884ea
Rushil Nagarsheth <rushil.nagarsheth@gmail.com>
2019-06-02 01:42:34 -0400

    Added Screenshots

---
+++ TicTac2Tests/TicTac2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  TicTac2Tests.swift
+//  TicTac2Tests
+//
+//  Created by Rushil Nagarsheth on 01/06/19.
+//  Copyright © 2019 Rushil Nagarsheth. All rights reserved.
+//
+
+import XCTest
+@testable import TicTac2
+
+class TicTac2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
